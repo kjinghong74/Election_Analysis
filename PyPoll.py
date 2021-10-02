0c5bc97048fc094501ab828bcfe77dbb94701524
Jinghong Kou <kjinghong@hotmail.com>
2021-10-02 14:56:55 -0400

    adding PyPoll.py file

---
+++ PyPoll.py
@@ -0,0 +1,17 @@
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     #print(election_data)
+
+     # Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+
+     #Print each row in the csv file
+    
+    headers = next(file_reader)
+    print(headers)
